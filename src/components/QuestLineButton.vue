9d089ff34c2f75b282d481d35c38737247ce6f99
Ibrahim Omer <ibrahimomer89@gmail.com>
2024-10-09 16:54:26 +0200

    Add Icons, Change fetch behaviour, Add Icon Scaling on zoom level, add questboard groundwork

---
+++ src/components/QuestLineButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="quest-line-button-wrapper">
+        Add Questline
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    .quest-line-button-wrapper{
+        background-color: #1d160f;
+        background-image: url("../assets/backgrounds/wood-pattern-horizontal.png");
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-family: 'MedievalSharp', Arial !important;
+        color: #d3dbdf;
+        font-size: 1.1rem;
+    }
+</style>
